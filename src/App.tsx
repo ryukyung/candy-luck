b04f629faf27d61ccb9912f83398ff8ec2a5138b
ryukyunghye <ryukyung123@naver.com>
2024-06-07 13:05:27 +0900

    :lipstick: (component/#9): 메인 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from '@emotion/styled';
+import Logo from './components/Logo';
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const clickHandler = () => console.log('뽑기');
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MainStyled>
+        <MainContentStyled>
+          <h1>Candy Luck</h1>
+          <Logo />
+          <Button clickHandler={clickHandler}>뽑기</Button>
+        </MainContentStyled>
+      </MainStyled>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+const MainStyled = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--ivory);
+`;
+
+const MainContentStyled = styled.section`
+  width: 390px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 80px;
+  background-color: var(--red);
+  h1 {
+    font-family: var(--title);
+    font-size: 40px;
+    color: var(--ivory);
+  }
+`;
